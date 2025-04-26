91ec72721f1bc9da8165e59200d48be9c39a59f8
yudai-yam <yudaimadeinjapan@gmail.com>
2025-04-26 12:48:01 +0200

    create eval demo

--- frontend/src/challenges/jquery_mobile.php
+++ frontend/src/challenges/jquery_mobile.php
@@ -12,18 +12,19 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
-    <script src="scripts/jquery_mobile.js"></script>
 </head>
 <body>
     <p>Make use of known gadgets in JQuery Mobile!</p>
+    <p>Your input</p>
     <p id="app"></p>
 
     <button id="toggle_examples">Toggle Examples</button>
     <div id="examples">
-        <p>
-            example: &lt;img src="x" onerror="alert('XSS')"&gt;
-        </p>
+        <p>hint: You can put anything with id=expression and attribute=data</p>
+        <p>example: &lt;div id="expression" data="alert(1)"&gt;&lt;/div&gt;</p>
+        <p>note: you might need to encode it</p>
     </div>
     <script src="../utils/show_hide.js"></script>
+    <script src="scripts/jquery_mobile.js"></script>
 </body>
 </html>
