f30a9530cd283e99516122c7dc7668c9985fb0ed
yudai-yam <yudaimadeinjapan@gmail.com>
2025-07-21 17:48:51 +0200

    first frontend backend combo works

--- frontend/input_reflection/secure/input-reflection.js
+++ frontend/input_reflection/secure/input-reflection.js
@@ -1,19 +1,18 @@
 /*
-simple javascript injection in innerHTML vulnerability
-
-example: <img src="x" onerror="alert('XSS')">
-*/
-// document.getElementById("submit").onclick = function(){
-//     var username = document.getElementById("input").value;
+simple javascript injection in innerHTML vulnerability example: <img src="x" onerror="alert('XSS')"> */ // document.getElementById("submit").onclick = function(){ //     var username = document.getElementById("input").value;
 //     var cleanUsername = DOMPurify.sanitize(username)
 //     document.getElementById("target").innerHTML = cleanUsername
 // }
 async function sendInput() {
-    const input = document.getElementById("inputBox").value;
+    const input = document.getElementById("input").value;
     const encodedInput = encodeURIComponent(input);
+    console.log(encodedInput)
+
+    let backend = await getConfigValue("backend")
+    console.log(backend)
 
     try {
-        const response = await fetch(`http://localhost:8000/challenges/input_reflection/${encodedInput}`, {
+        const response = await fetch(`${backend}/input_reflection/${encodedInput}`, {
             method: 'POST',
     });
 
