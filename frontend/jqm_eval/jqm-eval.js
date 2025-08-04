5ed7ae20e09cba403ed0f6cbe56fb304918b44c6
yudai-yam <yudaimadeinjapan@gmail.com>
2025-08-04 17:32:54 +0200

    use backend for jqm

--- frontend/jqm_eval/jqm-eval.js
+++ frontend/jqm_eval/jqm-eval.js
@@ -1,9 +1,35 @@
 var ref = document.location.href.split("?request=")[1];
-console.log(ref)
-document.getElementById("app").innerHTML = decodeURIComponent(ref);
 
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", async function() { 
+    var received = await sendInput(ref);
+
+    if (received) {
+        document.getElementById("result").innerHTML = decodeURIComponent(received);
+    }
+
     var expression = document.getElementById("expression").getAttribute("data");
     var parsed = eval(expression);
-    document.getElementById("result").innerHTML = '<p>'+parsed+'</p>';
+    document.getElementById("result").innerHTML += '<p>' + parsed + '</p>';
 });
+
+async function sendInput(userInput) {
+    const input = userInput || document.getElementById("input").value;
+    const encodedInput = encodeURIComponent(input);
+    console.log(encodedInput);
+
+    let backend = await getConfigValue("backend");
+    console.log(backend);
+
+    try {
+        const response = await fetch(`${backend}/api/input_reflection/${encodedInput}`, {
+            method: 'POST',
+        });
+
+        const data = await response.json();
+        console.log(data.reflected_input);
+        document.getElementById("result").innerHTML = decodeURIComponent(data.reflected_input);
+        return data.reflected_input;
+    } catch (error) {
+        document.getElementById("result").innerHTML = "Error: " + error;
+    }
+}
