e52db831c1f0532911d9216cf02a52dad2277564
yudai-yam <yudaimadeinjapan@gmail.com>
2025-08-07 16:15:37 +0200

    fix jqm popup

--- frontend/jqm_popup/jqm_popup.php
+++ frontend/jqm_popup/jqm_popup.php
@@ -8,6 +8,9 @@
     <title>JQuery Mobile Gadget (Popup)</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <script src="https://code.jquery.com/jquery-1.8.3.min.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css">
+    <script src="https://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
 </head>
 <body>
     <h1 id="main_header">JQuery Mobile Popup Gadget</h1>
@@ -17,15 +20,15 @@
     <p id="app"></p>
     <div id="debug"></div>
 
-    <button id="toggle_examples">Toggle Examples</button>
+    <hr>
     <div id="examples">
-        <p>hint: You can make use of popup gadget in Jquery Mobile</p>
-        <p>example: &lt;div data-role=popup id='--!&gt;&lt;script&gt;alert("XSS")&lt;/script&gt;'&gt;&lt;/div&gt;<p>
+        <p>
+            hint: You can make use of popup gadget in Jquery Mobile
+        </p>
+        <p>
+            example: &lt;div data-role=popup id='--!&gt;&lt;script&gt;alert("XSS")&lt;/script&gt;'&gt;&lt;/div&gt;
+        </p>
     </div>
-    <script src="../utils/show-hide.js"></script>
     <script src="./jqm-popup.js"></script>
-    <script src="https://code.jquery.com/jquery-1.8.3.min.js"></script>
-    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css">
-    <script src="https://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
 </body>
 </html>
